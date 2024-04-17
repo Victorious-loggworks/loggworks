12ebc2f4258e5a340680053c7249e00a24b74e80
victornwakpa <nwakpavictor@gmail.com>
2024-04-17 09:49:16 +0100

    added video playback

--- app/components/Home/Client.tsx
+++ app/components/Home/Client.tsx
@@ -1,32 +1,56 @@
 import data from "@/app/data/listcard";
 import Image from "next/image.js";
 
-
 export default function Client() {
-    return (
-        <div className="px-20 container">
-            <div className="flex flex-col items-center">
-                <h2 className="text-primary-20 text-xl uppercase text-center font-semibold">FOR CLIENTS</h2>
-                <p className="text-center text-gray-10 text-[40px] font-bold leading-[48px] capitalize mt-6">Discover, Compare, and Hire</p>
-                <p className="text-xl mt-4 text-gray-30">Find Your Perfect Professional with Loggworks</p>
+  return (
+    <div className="px-20 container">
+      <div className="flex flex-col items-center">
+        <h2 className="text-primary-20 text-xl uppercase text-center font-semibold">
+          FOR CLIENTS
+        </h2>
+        <p className="text-center text-gray-10 text-[40px] font-bold leading-[48px] capitalize mt-6">
+          Discover, Compare, and Hire
+        </p>
+        <p className="text-xl mt-4 text-gray-30">
+          Find Your Perfect Professional with Loggworks
+        </p>
 
-                <div className="my-[72px] w-full h-[674px] bg-gray-300">
-                </div>
-            </div>
-
-            <div>
-                <div className="hidden lg:grid grid-cols-3 lg:grid-cols-3 gap-6 gap-y-6 mt-12">
-                    {data.map((image) => (
-                        <div key={image.id} className="flex flex-col gap-2 justify-center p-6 bg-gray-card rounded-2xl">
-                            <div className="">
-                                <Image src={image.src} alt={image.title} width={48} height={48} />
-                            </div>
-                            <h3 className="text-xl font-semibold text-gray-10">{image.title}</h3>
-                            <p className="text-sm font-medium text-gray-30">{image.desc}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
+        <div className="w-full h-[674px] my-[72px] rounded-xl overflow-hidden">
+          <video
+            autoPlay
+            loop
+            muted
+            style={{ width: "100%", height: "100%" }}
+            className="h-fit object-cover"
+          >
+            <source src="videos/loggworks-video.mp4" />
+          </video>
         </div>
-    );
+      </div>
+
+      <div>
+        <div className="hidden lg:grid grid-cols-3 lg:grid-cols-3 gap-6 gap-y-6 mt-12">
+          {data.map((image) => (
+            <div
+              key={image.id}
+              className="flex flex-col gap-2 justify-center p-6 bg-gray-card rounded-2xl"
+            >
+              <div className="">
+                <Image
+                  src={image.src}
+                  alt={image.title}
+                  width={48}
+                  height={48}
+                />
+              </div>
+              <h3 className="text-xl font-semibold text-gray-10">
+                {image.title}
+              </h3>
+              <p className="text-sm font-medium text-gray-30">{image.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
